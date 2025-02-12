fd06e529ce4ed37c62da6d45da803176f1b79a80
Shilakem <sheilahkemunto501517@gmail.com>
2025-02-12 13:22:24 +0300

    Update Resume Script.sql

--- Resume Script.sql
+++ Resume Script.sql
@@ -19,6 +19,10 @@ SELECT *
 FROM jobroles
 WHERE JobID BETWEEN 5 AND 10;
 
+SELECT *
+FROM jobroles
+WHERE JobID NOT BETWEEN 5 AND 10;
+
 SELECT jobroles.CompanyName,jobroles.Location
 FROM jobroles
 WHERE Location LIKE 'S%';
@@ -27,6 +31,10 @@ SELECT*
 FROM jobroles
 WHERE Location IN ('Austin','Seattle');
 
+SELECT*
+FROM jobroles
+WHERE Location NOT IN ('Austin','Seattle');
+
 SELECT *
 FROM jobroles
 ORDER BY Salary DESC;
@@ -34,6 +42,10 @@ ORDER BY Salary DESC;
 SELECT *
 FROM jobroles
 WHERE JobType='Remote' AND (Location LIKE 'S%' OR Location LIKE '%S%');
+
+SELECT*
+FROM jobroles
+WHERE JobType LIKE '____-t%'; -- wildcards
 
 SELECT *
 FROM jobroles
@@ -57,3 +69,35 @@ WHERE CompanyName IS NULL;
 UPDATE jobroles
 SET CompanyName='Cigna',Applications='120'
 WHERE jobroles.JobID=21;
+
+DELETE FROM jobroles
+WHERE CompanyName='Cigna';
+
+SELECT *
+FROM jobroles
+WHERE Salary>80000
+ORDER BY Salary DESC
+LIMIT 3;
+
+SELECT COUNT(DISTINCT jobroles.Location) AS Total_numof_Locations
+FROM jobroles;
+
+SELECT COUNT(DISTINCT jobroles.Location) AS "Total number of Locations"
+FROM jobroles;
+
+SELECT MIN(SALARY)
+FROM jobroles;
+
+SELECT MAX(SALARY)
+FROM jobroles;
+
+SELECT JobTitle,SUM(Applications) AS TOTAL_APPLICATIONS
+FROM jobroles
+GROUP BY JobTitle
+ORDER BY SUM(Applications) DESC;
+
+SELECT Jobtitle,SUM(Applications)
+FROM jobroles
+GROUP BY Jobtitle
+HAVING SUM(Applications) > 600
+ORDER BY JobTitle;
